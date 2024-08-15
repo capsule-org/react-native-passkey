8885d217faef8a84939cd4ec982deda7fcdb31af
Jordy McNab <jordy@usecapsule.com>
2024-08-15 09:25:28 -0500

    fix: fixes missing data types (#2)
    * Adding extensions for base64 coding
    
    * updating package name
    
    * wip
    
    * chore: release 2.1.3
    
    * chore: Bump version to 2.2.0-dev.1
    
    * chore: fix missing credential and base64 data
    
    * chore: Bum version to 2.2.0-dev.2
    
    * fix: missing Data type
    
    * chore: bumb version 2.2.0-dev.3
    
    * chore: bumb version 2.2.0-dev.4
    
    * chore: pod version bump
    
    * chore: Bump version to 2.2.0
    
    ---------
    
    Co-authored-by: Brian Corbin <brian.william.corbin@gmail.com>

--- ios/Passkey.swift
+++ ios/Passkey.swift
@@ -1,4 +1,5 @@
 import AuthenticationServices
+import Foundation
 
 @objc(Passkey)
 class Passkey: NSObject {
@@ -6,12 +7,15 @@ class Passkey: NSObject {
 
   @objc(register:withChallenge:withDisplayName:withUserId:withSecurityKey:withResolver:withRejecter:)
   func register(_ identifier: String, challenge: String, displayName: String, userId: String, securityKey: Bool, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-    // Convert challenge and userId to correct type
-    guard let challengeData: Data = Data(base64Encoded: challenge) else {
-      reject(PassKeyError.invalidChallenge.rawValue, PassKeyError.invalidChallenge.rawValue, nil);
-      return;
+    guard let challengeData: Data = Data.fromBase64URL(challenge) else {
+        reject(PassKeyError.invalidChallenge.rawValue, PassKeyError.invalidChallenge.rawValue, nil);
+        return
     }
-    let userIdData: Data = RCTConvert.nsData(userId);
+
+    guard let userIdData: Data = Data.fromBase64URL(userId) else {
+        reject(PassKeyError.invalidUserId.rawValue, PassKeyError.invalidUserId.rawValue, nil);
+        return
+    }
 
     // Check if Passkeys are supported on this OS version
     if #available(iOS 15.0, *) {
@@ -44,12 +48,12 @@ class Passkey: NSObject {
         if let registrationResult = result?.registrationResult {
           // Return a NSDictionary instance with the received authorization data
           let authResponse: NSDictionary = [
-            "rawAttestationObject": registrationResult.rawAttestationObject.base64EncodedString(),
-            "rawClientDataJSON": registrationResult.rawClientDataJSON.base64EncodedString()
+            "rawAttestationObject": registrationResult.rawAttestationObject.toBase64URL(),
+            "rawClientDataJSON": registrationResult.rawClientDataJSON.toBase64URL()
           ];
 
           let authResult: NSDictionary = [
-            "credentialID": registrationResult.credentialID.base64EncodedString(),
+            "credentialID": registrationResult.credentialID.toBase64URL(),
             "response": authResponse
           ]
           resolve(authResult);
@@ -69,75 +73,76 @@ class Passkey: NSObject {
     }
   }
 
-  @objc(authenticate:withChallenge:withSecurityKey:withResolver:withRejecter:)
-  func authenticate(_ identifier: String, challenge: String, securityKey: Bool, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+  @objc(authenticate:withChallenge:withSecurityKey:withCredentialId:withResolver:withRejecter:)
+func authenticate(_ identifier: String, challenge: String, securityKey: Bool, credentialId: String?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
 
-    // Convert challenge to correct type
-    guard let challengeData: Data = Data(base64Encoded: challenge) else {
-      reject(PassKeyError.invalidChallenge.rawValue, PassKeyError.invalidChallenge.rawValue, nil);
-      return;
-    }
-
-    // Check if Passkeys are supported on this OS version
-    if #available(iOS 15.0, *) {
-      let authController: ASAuthorizationController;
-
-      // Check if authentication should proceed with a security key
-      if (securityKey) {
-        // Create a new assertion request with security key
-        let securityKeyProvider = ASAuthorizationSecurityKeyPublicKeyCredentialProvider(relyingPartyIdentifier: identifier);
-        let authRequest = securityKeyProvider.createCredentialAssertionRequest(challenge: challengeData);
-        authController = ASAuthorizationController(authorizationRequests: [authRequest]);
-      } else {
-          // Create a new assertion request without security key
-          let platformProvider = ASAuthorizationPlatformPublicKeyCredentialProvider(relyingPartyIdentifier: identifier);
-          let authRequest = platformProvider.createCredentialAssertionRequest(challenge: challengeData);
-          
-          if let credentialId = credentialId {
-            authRequest.allowedCredentials = [ASAuthorizationPlatformPublicKeyCredentialDescriptor(credentialID: Data.fromBase64Url(credentialId)!)]
-          }
-          
-          authController = ASAuthorizationController(authorizationRequests: [authRequest]);
-      }
-
-      // Set up a PasskeyDelegate instance with a callback function
-      self.passKeyDelegate = PasskeyDelegate { error, result in
-        // Check if authorization process returned an error and throw if thats the case
-        if (error != nil) {
-          let passkeyError = self.handleErrorCode(error: error!);
-          reject(passkeyError.rawValue, passkeyError.rawValue, nil);
-          return;
-        }
-        // Check if the result object contains a valid authentication result
-        if let assertionResult = result?.assertionResult {
-          // Return a NSDictionary instance with the received authorization data
-          let authResponse: NSDictionary = [
-            "rawAuthenticatorData": assertionResult.rawAuthenticatorData.base64EncodedString(),
-            "rawClientDataJSON": assertionResult.rawClientDataJSON.base64EncodedString(),
-            "signature": assertionResult.signature.base64EncodedString(),
-          ];
-
-          let authResult: NSDictionary = [
-            "credentialID": assertionResult.credentialID.base64EncodedString(),
-            "userID": String(decoding: assertionResult.userID, as: UTF8.self),
-            "response": authResponse
-          ]
-          resolve(authResult);
-        } else {
-          // If result didn't contain a valid authentication result throw an error
-          reject(PassKeyError.requestFailed.rawValue, PassKeyError.requestFailed.rawValue, nil);
-        }
-      }
-
-      if let passKeyDelegate = self.passKeyDelegate {
-        // Perform the authorization request
-        passKeyDelegate.performAuthForController(controller: authController);
-      }
-    } else {
-      // If Passkeys are not supported throw an error
-      reject(PassKeyError.notSupported.rawValue, PassKeyError.notSupported.rawValue, nil);
-    }
+  // Convert challenge to correct type
+  guard let challengeData = Data.fromBase64URL(challenge) else {
+    reject(PassKeyError.invalidChallenge.rawValue, PassKeyError.invalidChallenge.rawValue, nil);
+    return
   }
+
+  // Check if Passkeys are supported on this OS version
+  if #available(iOS 15.0, *) {
+    let authController: ASAuthorizationController;
+
+    // Check if authentication should proceed with a security key
+    if (securityKey) {
+      // Create a new assertion request with security key
+      let securityKeyProvider = ASAuthorizationSecurityKeyPublicKeyCredentialProvider(relyingPartyIdentifier: identifier);
+      let authRequest = securityKeyProvider.createCredentialAssertionRequest(challenge: challengeData);
+      authController = ASAuthorizationController(authorizationRequests: [authRequest]);
+    } else {
+      // Create a new assertion request without security key
+      let platformProvider = ASAuthorizationPlatformPublicKeyCredentialProvider(relyingPartyIdentifier: identifier);
+      let authRequest = platformProvider.createCredentialAssertionRequest(challenge: challengeData);
+      
+      // Add allowed credentials if credentialId is provided
+      if let credentialId = credentialId, let credentialData = Data.fromBase64URL(credentialId) {
+        authRequest.allowedCredentials = [ASAuthorizationPlatformPublicKeyCredentialDescriptor(credentialID: credentialData)];
+      }
+      
+      authController = ASAuthorizationController(authorizationRequests: [authRequest]);
+    }
+
+    // Set up a PasskeyDelegate instance with a callback function
+    self.passKeyDelegate = PasskeyDelegate { error, result in
+      // Check if authorization process returned an error and throw if thats the case
+      if (error != nil) {
+        let passkeyError = self.handleErrorCode(error: error!);
+        reject(passkeyError.rawValue, passkeyError.rawValue, nil);
+        return;
+      }
+      // Check if the result object contains a valid authentication result
+      if let assertionResult = result?.assertionResult {
+        // Return a NSDictionary instance with the received authorization data
+        let authResponse: NSDictionary = [
+          "rawAuthenticatorData": assertionResult.rawAuthenticatorData.toBase64URL(),
+          "rawClientDataJSON": assertionResult.rawClientDataJSON.toBase64URL(),
+          "signature": assertionResult.signature.toBase64URL(),
+        ];
+
+        let authResult: NSDictionary = [
+          "credentialID": assertionResult.credentialID.toBase64URL(),
+          "userID": assertionResult.userID.toBase64URL(),
+          "response": authResponse
+        ]
+        resolve(authResult);
+      } else {
+        // If result didn't contain a valid authentication result throw an error
+        reject(PassKeyError.requestFailed.rawValue, PassKeyError.requestFailed.rawValue, nil);
+      }
+    }
+
+    if let passKeyDelegate = self.passKeyDelegate {
+      // Perform the authorization request
+      passKeyDelegate.performAuthForController(controller: authController);
+    }
+  } else {
+    // If Passkeys are not supported throw an error
+    reject(PassKeyError.notSupported.rawValue, PassKeyError.notSupported.rawValue, nil);
+  }
+}
 
   // Handles ASAuthorization error codes
   func handleErrorCode(error: Error) -> PassKeyError {
@@ -159,7 +164,8 @@ enum PassKeyError: String, Error {
   case notSupported = "NotSupported"
   case requestFailed = "RequestFailed"
   case cancelled = "UserCancelled"
-  case invalidChallenge = "InvalidChallenge"
+  case invalidChallenge = "InvalidChallenge" 
+  case invalidUserId = "InvalidUserId"
   case notConfigured = "NotConfigured"
   case unknown = "UnknownError"
 }
@@ -196,4 +202,57 @@ struct PassKeyAssertionResult {
 enum PasskeyOperation {
   case Registration
   case Assertion
+}
+
+public extension Data {
+    /// Same as ``Data(base64Encoded:)``, but adds padding automatically
+    /// (if missing, instead of returning `nil`).
+    static func fromBase64(_ encoded: String) -> Data? {
+        // Prefixes padding-character(s) (if needed).
+        var encoded = encoded
+        let remainder = encoded.count % 4
+        if remainder > 0 {
+            encoded = encoded.padding(
+                toLength: encoded.count + 4 - remainder,
+                withPad: "=", startingAt: 0
+            )
+        }
+
+        // Finally, decode.
+        return Data(base64Encoded: encoded)
+    }
+
+    static func fromBase64URL(_ encoded: String) -> Data? {
+        let base64String = base64UrlToBase64(base64Url: encoded)
+        return fromBase64(base64String)
+    }
+
+    private static func base64UrlToBase64(base64Url: String) -> String {
+        let base64 = base64Url
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+
+        return base64
+    }
+}
+
+public extension String {
+    static func fromBase64(_ encoded: String) -> String? {
+        if let data = Data.fromBase64(encoded) {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+}
+
+extension Data {
+    func toBase64URL() -> String {
+        let current = self
+
+        var result = current.base64EncodedString()
+        result = result.replacingOccurrences(of: "+", with: "-")
+        result = result.replacingOccurrences(of: "/", with: "_")
+        result = result.replacingOccurrences(of: "=", with: "")
+        return result
+    }
 }
